c6979a934a98cbf244aab73d47863688892f67dc
onachassi <onachassi@gmail.com>
2017-05-11 14:20:33 -0700

    got highlighting to work

--- application.js
+++ application.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   
+$("body").find("*").hover(function(){
+		$(this).css("box-shadow","0px 0px 5px 0px rgba(0, 255, 0, 0.67)")
+		$(this).click(function(){
+			$("._Jig > div > span").css("background-color", "green")
+		})
+	},function(){
+		$(this).css("box-shadow", "none")
+})
+
+  
 });
 
 
