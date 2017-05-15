f34be69bfc9e25af9c07c4dd1cb26a98b7c66d7e
onachassi <onachassi@gmail.com>
2017-05-15 13:14:47 -0700

    added input section need to hook it up

--- application.js
+++ application.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+
+	var cssChanges = {
+
+	}
   
 $("body").find("*").hover(function(){
 		$(this).css("box-shadow","0px 0px 5px 0px rgba(0, 255, 0, 0.67)")
 		$(this).click(function(e){
-			e.stopPropagation();
+			e.stopPropagation(); //stop after finding the element selector
 			var selector = $(this)
             .parents()
             .map(function() { return this.tagName; })
@@ -27,6 +31,9 @@ $("body").find("*").hover(function(){
          	selector += ":nth-child(" + nthChild + ")"
          }
 
+        $('#cssinputhelper').remove()
+        $(this).append(showInput(selector))
+        $('*').off('click')
         return console.log(selector)
 
     })
@@ -34,6 +41,18 @@ $("body").find("*").hover(function(){
 		$(this).css("box-shadow", "none")
 })
 
+var showInput = function(selector){
+	var input = `<div id='cssinputhelper' style="display:flex; flex-direction:column; margin: 20px; border: 2px solid black; padding: 5px; justify-content: center; align-items: center;">
+								<label for="selector">${selector}</label>
+								<div>
+									<input type="text" placeholder="Property" />
+									<input type="text" placeholder="Value" />
+									<button>check</button>
+									<button>ex</button>
+								</div>
+							</div>`
+	return input
+}
 
 });
 
